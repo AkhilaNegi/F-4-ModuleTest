7226c37195829ff897d3f52cf657219e8f615a2a
Akhila Negi <122104289+AkhilaNegi@users.noreply.github.com>
2023-08-05 23:45:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,129 +1,164 @@
-const getBtn = document.getElementById('get-data');
-const infoContainer = document.querySelector('.info-container');
-const pinContainer = document.querySelector('.pin-container');
-const postOfficeContainer = document.querySelector('.post-container');
-const lat = document.getElementById('lat');
-const long = document.getElementById('long');
-const city = document.getElementById('city');
-const region = document.getElementById('region');
-const organisation = document.getElementById('organisation');
-const hostname = document.getElementById('hostname');
-const map = document.getElementById('map');
-const x = document.getElementById('x');
-const timeZone = document.getElementById('time-zone');
-const dateTime = document.getElementById('date-time');
-const pincode = document.getElementById('pincode');
-const message = document.getElementById('message');
-const search = document.getElementById('search');
+const YourIp = document.getElementById("yourIp");
+let ip_address = null;
+function getIP() {
+  fetch("https://api.ipify.org?format=json")
+    .then((response) => response.json())
+    .then((data) => {
+      ip_address = data.ip;
 
-
-var IP;
-var dataJson;
-var latitude;
-var longitude;
-var pin;
-
-var postOfficeArr=[];
-
-fetch('https://api.ipify.org?format=json')
-.then((resp)=>resp.json())
-.then((data)=>{
-    console.log(data.ip);
-    IP=data.ip;
-    document.getElementById('ip').innerText = data.ip;
-}).catch((e)=>{
-    console.log("Error while fetching Ip address",e);
-})
-
-getBtn.addEventListener('click',()=>{
-        console.log('ehllpo');
-        // 6cb6e85b8cb4a2
-        setTimeout(() => {
-            fetch(`https://ipinfo.io/${IP}/geo`)
-            .then((resp)=>resp.json())
-            .then((data)=>{
-                dataJson=data;
-                console.log("data",dataJson);
-                let location=dataJson.loc;
-                latitude = 30.293461;
-                longitude = 78.524094;
-                lat.innerHTML = `<strong>Lat: </strong>${latitude}`;
-                long.innerHTML = `<strong>Long: </strong>${longitude}`;
-                city.innerHTML = `<strong>City: </strong>${dataJson.city}`;
-                region.innerHTML = `<strong>Region: </strong>${dataJson.region}`;
-                organisation.innerHTML = `<strong>Organisation: </strong>${dataJson.org}`;
-                hostname.innerHTML = `<strong>Hostname: </strong>${dataJson.hostname}`;
-    
-                map.setAttribute('src',`https://maps.google.com/maps?q=30.293461,78.524094&hl=en&z=14&amp&output=embed`);
-    
-                let datetime_str = new Date().toLocaleString("en-US", { timeZone: `${dataJson.timezone}` });
-                timeZone.innerHTML = `<strong>Time Zone: </strong>${dataJson.timezone}`;
-                dateTime.innerHTML = `<strong>Date And Time: </strong>${datetime_str}`;
-                pin = dataJson.postal;
-                pincode.innerHTML = `<strong>Pincode: </strong>${dataJson.postal}`;
-                postOffice(pin);
-    
-            })
-            .catch((e)=>console.log('Error',e))
-
-           
-
-        }, 1000);
-
-
-        getBtn.style.display='none';
-        infoContainer.style.display='flex';
-        pinContainer.style.display='block';
-})
-
-function postOffice(pin) {
-    console.log(pin);
-    fetch(`https://api.postalpincode.in/pincode/${pin}`)
-    .then((resp)=>resp.json())
-    .then((data)=>{
-        console.log(data[0]);
-        message.innerHTML = `<strong>Message: </strong>${data[0].Message}`;
-        console.log(data[0].PostOffice);
-        postOfficeArr = data[0].PostOffice;
-        search.style.display='block';
-
-        showPostOffice(postOfficeArr);
+      console.log("Your public IP address is:", ip_address);
+      YourIp.innerHTML = ip_address;
     })
-    .catch((e)=>{
-        console.log("Error",e);
-    })
-    
+    .catch((error) => {
+      console.error("Error fetching IP:", error);
+    });
 }
 
-function showPostOffice(Arr) {
-    postOfficeContainer.innerHTML='';
-    let myHtml='';
-    Arr.forEach((ele)=>{
-        myHtml+=`
-        <div class="post-content">
-         <div><strong>Name:</Strong> ${ele.Name}</div>
-         <div><strong>Branch Type:</Strong> ${ele.BranchType}</div>
-         <div><strong>Delivery Status:</Strong> ${ele.DeliveryStatus}</div>
-         <div><strong>District:</Strong> ${ele.District}</div>
-         <div><strong>Division:</Strong> ${ele.Division}</div>
-        </div>
-        `
-    })
-    postOfficeContainer.innerHTML=myHtml;
-}
+getIP();
+var LocationData;
 
+const container = document.querySelector(".container");
+const post = document.getElementById("post");
+const dataDiv = document.getElementById("dataDiv");
+const infoDiv = document.getElementById("info-div");
+var locationData;
+var postofficeData;
 
-search.addEventListener('input',()=>{
-    var filterArr = postOfficeArr.filter((ele)=>{
-        // return ele.toLocation.toLowerCase().includes(search.value.trim().toLowerCase());
-        if(ele.Name.toLowerCase().includes(search.value.trim().toLowerCase())){
-            return ele;
-        }
-    })
-    showPostOffice(filterArr);
-})
+var postalCard;
 
+async function ShowData() {
+  container.style.display = "none";
+  post.style.display = "block";
 
+  var IPAddress;
 
+  await $.getJSON("https://api.ipify.org?format=json", function (data) {
+    IPAddress = data.ip;
+  });
 
+  console.log(IPAddress);
+
+  await fetch(`https://ipinfo.io/${IPAddress}?token=30dd7efacbc702`)
+    .then((response) => response.json())
+    .then((response) => (locationData = response))
+    .catch(() => {
+      alert("Problem with fetching data");
+    });
+  console.log(locationData);
+
+  var lagLong = locationData.loc.split(",");
+  var lat = lagLong[0].trim();
+  var long = lagLong[1].trim();
+  console.log(lat, long);
+  // ------------Fatch Data and map-------
+  dataDiv.innerHTML += `
   
+   <nav>
+        <div class="nav">
+          <h1>IP Address : <span id="IpAdd">127.0.01</span></h1>
+
+          <div class="information">
+            <div class="lat">
+              <h1>Lat:${"  " + lat}</h1>
+              <h1>Long:${"  " + long}</h1>
+            </div>
+            <div id="city">
+              <h1>City:${"  " + locationData.city}</h1>
+              <h1>Region:${"  " + locationData.region}</h1>
+            </div>
+            <div id="organisation">
+              <h1>Organisation:${"  " + locationData.org}</h1>
+              <h1>Hostname:${"  "}Akhila Negi</h1>
+            </div>
+          </div>
+        </div>
+        <div class="map">
+          <h1>Your Current Location</h1>
+          <iframe
+          src="https://maps.google.com/maps?q=30.293461,78.524094&z=15&output=embed"
+          frameborder="0"
+          id="mapFrame"
+        ></iframe>
+        </div>
+      </nav>
+  `;
+
+  // ------------Time and postal Addresh-------
+  let datetime_str = new Date().toLocaleString("en-US", {
+    timeZone: `${locationData.timezone}`,
+  });
+
+  await fetch(`https://api.postalpincode.in/pincode/${locationData.postal} `)
+    .then((res) => res.json())
+    .then((res) => res[0])
+    .then((res) => {
+      console.log("postal data" + res);
+
+      infoDiv.innerHTML += `
+      <h1 id="info">More Information About You</h1>
+          <div class="about-info">
+            <h1>Time Zone:${"  " + locationData.timezone}</h1>
+            <h1>Date And Time:${"  " + datetime_str}</h1>
+            <h1>Pincode:${"  " + locationData.postal}</h1>
+              <h1> Message:${"  " + res.Message}</h1>
+          </div>
+       <div class="postOffice">
+          <h1>Post Offices Near You</h1>
+          <div class="box">
+            <input type="search" name="" id="searchBox" onkeyup="searchKey()"/>
+            <img src="./Vector (3).png" alt="" />
+          </div>
+        </div>
+        <div id="postalCard"></div>
+      `;
+      postalCard = document.getElementById("postalCard");
+      return res.PostOffice;
+    })
+    .then((data) => {
+      console.log(data);
+
+      postofficeData = data;
+
+      data.forEach((element) => {
+        console.log(element);
+        postalCard.innerHTML += `
+        <div class="card">
+            <div class="name">Name:${" " + element.Name}</div>
+            <div class="Type">Branch Type:${" " + element.BranchType}</div>
+            <div class="status">Delivery Status:${
+              " " + element.DeliveryStatus
+            }</div>
+            <div class="dis">District:${" " + element.District}</div>
+            <div class="div">Division:${" " + element.Division}</div>
+          </div>
+        `;
+      });
+    })
+    .catch(() => {
+      alert("Problem with fetching data");
+    });
+}
+
+function searchKey() {
+  postalCard.innerHTML = "";
+  var searchValue = document.getElementById("searchBox").value;
+  // console.log(searchValue);
+
+  var searchFilter = postofficeData.filter((item) => {
+    var filterValue = JSON.stringify(item);
+    return filterValue.toLowerCase().includes(searchValue.toLowerCase());
+  });
+  searchFilter.forEach((element) => {
+    postalCard.innerHTML += `
+        <div class="card">
+            <div class="name">Name:${" " + element.Name}</div>
+            <div class="Type">Branch Type:${" " + element.BranchType}</div>
+            <div class="status">Delivery Status:${
+              " " + element.DeliveryStatus
+            }</div>
+            <div class="dis">District:${" " + element.District}</div>
+            <div class="div">Division:${" " + element.Division}</div>
+          </div>
+        `;
+  });
+}
